eae28abe3c133ce2b81675d0eb0cb6080e5c0b94
Kaan Ersan <kanersan@gmail.com>
2018-09-28 22:01:20 -0400

    Some more basic work (creation of viewcontrollers, etc.)

---
+++ Samantha Evil/Samantha Evil/VideoPlayerViewController.swift
@@ -0,0 +1,27 @@
+//
+//  VideoPlayerViewController.swift
+//  Samantha Evil
+//
+//  Created by Kaan Ersan on 2018-09-28.
+//  Copyright © 2018 Kaan Ersan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        let url = NSURL(
+//        let player = AVPlayer(URL: url)
+//        let playerController = AVPlayerViewController()
+//        playerController.player = player
+//        self.addChildViewController(playerController)
+//        self.view.addSubview(playerController.view)
+//        playerController.view.frame = self.view.frame
+//        player.play()
+    }
+
+}
