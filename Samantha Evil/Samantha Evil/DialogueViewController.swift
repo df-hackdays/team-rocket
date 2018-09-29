2f74f05ab6ab8d2bae4ad2b93024d362028e519d
Samuel Chung <samuelminghim.chung@scotiabank.com>
2018-09-29 03:59:36 -0400

    implemented button to open Url
    - fixed logic to disable previous selected button

--- Samantha Evil/Samantha Evil/DialogueViewController.swift
+++ Samantha Evil/Samantha Evil/DialogueViewController.swift
@@ -20,6 +20,10 @@ class DialogueViewController: UIViewController, UITableViewDataSource, UITableVi
         case message, button
     }
     
+    enum ButtonType {
+        case nextMessages, openUrl
+    }
+    
     struct Dialogue {
         var dialogue: String
         var buttonA: Button? //receive chat message pack #
@@ -31,6 +35,7 @@ class DialogueViewController: UIViewController, UITableViewDataSource, UITableVi
     struct Button {
         var buttonLabel: String
         var buttonAction: Int
+        var type: ButtonType
     }
     
     override func viewDidLoad() {
@@ -50,6 +55,10 @@ class DialogueViewController: UIViewController, UITableViewDataSource, UITableVi
                 let indexPath = IndexPath(row: self.shownDialogueArray.count - 1, section: 0)
                 self.dialogueTableView.insertRows(at: [indexPath], with: UITableViewRowAnimation.fade)
                 self.dialogueTableView.endUpdates()
+                self.dialogueTableView.layoutIfNeeded()
+                self.dialogueTableView.scrollToRow(at: indexPath,
+                                           at: UITableViewScrollPosition.bottom,
+                                           animated: true)
             }
         }
 
@@ -59,31 +68,96 @@ class DialogueViewController: UIViewController, UITableViewDataSource, UITableVi
         return shownDialogueArray.count
     }
     
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "chiefCell", for: indexPath) as? ChiefTableViewCell
-        
         let dialogue = shownDialogueArray[indexPath.row]
         
-        cell?.dialogueLabel.text = dialogue.dialogue
-        cell?.alpha = 0.0
-        
-        UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseIn, animations: {
-            cell?.alpha = 1.0
-        }, completion: nil)
-        
-        return cell!
+        switch dialogue.type {
+        case .message:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as? MessageTableViewCell
+            cell?.dialogueLabel.text = dialogue.dialogue
+            cell?.alpha = 0.0
+            
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseIn, animations: {
+                cell?.alpha = 1.0
+            }, completion: nil)
+            return cell!
+        default:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "buttonCell", for: indexPath) as? ButtonTableViewCell
+            
+            cell?.buttonA.setTitle(dialogue.buttonA?.buttonLabel, for: .normal)
+            cell?.buttonA.messagePack = dialogue.buttonA?.buttonAction
+            cell?.buttonA.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
+            cell?.buttonA.buttonKind = dialogue.buttonA?.type
+            cell?.buttonA.cell = cell
+            
+            cell?.buttonB.setTitle(dialogue.buttonB?.buttonLabel, for: .normal)
+            cell?.buttonB.messagePack = dialogue.buttonB?.buttonAction
+            cell?.buttonB.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
+            cell?.buttonB.buttonKind = dialogue.buttonB?.type
+            cell?.buttonB.cell = cell
+            
+            if indexPath.row == shownDialogueArray.count - 1 {
+                cell?.buttonA.isEnabled = true
+                cell?.buttonB.isEnabled = true
+            }
+            else {
+                cell?.buttonA.isEnabled = false
+                cell?.buttonB.isEnabled = false
+            }
+            
+            cell?.alpha = 0.0
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseIn, animations: {
+                cell?.alpha = 1.0
+            }, completion: nil)
+            return cell!
+        }
     }
     
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 120
+        let dialogue = shownDialogueArray[indexPath.row]
+        switch dialogue.type {
+        case .message:
+            return 120
+        default:
+            return 240
+        }
+    }
+    
+    @objc func buttonAction(_ sender: ChoiceButton!) {
+        
+        switch sender.buttonKind {
+        case .openUrl?:
+            //open url
+            let url = URL(string: "http://www.youtube.com")
+            UIApplication.shared.open(url!, options: [:]) { (completed) in
+                //go to url
+            }
+        default:
+            dialogueArray = ChatMessages.getMessagePack(pack: sender.messagePack!)
+            showDialogue()
+            sender.cell?.buttonA.isEnabled = false
+            sender.cell?.buttonB.isEnabled = false
+        }
     }
 }
 
-class ChiefTableViewCell: UITableViewCell {
+class MessageTableViewCell: UITableViewCell {
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var dialogueLabel: UILabel!
+}
+
+class ButtonTableViewCell: UITableViewCell {
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var buttonA: ChoiceButton!
+    @IBOutlet weak var buttonB: ChoiceButton!
+    var choiceSelected: Bool = false
+}
+
+class ChoiceButton: UIButton {
+    var messagePack: Int?
+    var cell: ButtonTableViewCell?
+    var buttonKind: DialogueViewController.ButtonType?
 }
 
 
