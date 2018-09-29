111180a88196e01a379a9167ba4dacf4014851b9
Kaan Ersan <kanersan@gmail.com>
2018-09-28 21:32:29 -0400

    Pushing to master like a boss! First app commit.

---
+++ Samantha Evil/Samantha Evil/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Samantha Evil
+//
+//  Created by Kaan Ersan on 2018-09-28.
+//  Copyright © 2018 Kaan Ersan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
